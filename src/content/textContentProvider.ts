558448f78d211b282db7f278c9d2994fdf2eca23
Tobias Faller <fallert@tf.uni-freiburg.de>
2024-01-06 11:02:33 +0100

    Updated regex for stripping escape sequences from text. Fixes #31

--- src/content/textContentProvider.ts
+++ src/content/textContentProvider.ts
@@ -13,13 +13,17 @@ export class TextContentProvider implements vscode.Disposable {
 	}
 
 	public async provideTextDocumentContent(document: vscode.TextDocument, callback: (data: string) => Promise<void>): Promise<void> {
-		// eslint-disable-next-line no-control-regex
-		const escapeRegex = /\x1B\[((?:[0-9]+;)*[0-9]+)m/g;
-
 		for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
-			const line = document.lineAt(lineNumber);
+			const text = document.lineAt(lineNumber).text;
 
-			await callback(line.text.replace(escapeRegex, '') + '\n');
+			// See http://ascii-table.com/ansi-escape-sequences-vt-100.php
+			// And https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+			// eslint-disable-next-line no-control-regex
+			const stripped = text.replace(/\x1B\[([0-9?]*[hl]|[0-9;]*[mrHfy]|[0-9]*[ABCDgKJnqi]|[0-9;?]*[c])/g, '')
+				// eslint-disable-next-line no-control-regex
+				.replace(/\x1B([NODMEHc<=>FGABCDHIKJ]|[()][AB012]|#[0-9]|[0-9;]R|\/?Z|[0-9]+|O[PQRSABCDpqrstuvwxymlnM])/g, '');
+
+			await callback(`${stripped}\n`);
 		}
 	}
 
